d9ce431566372235b304167b90636269a529e753
Gustav Eiman <gustav.eiman@oneagency.se>
2018-05-11 14:38:49 +0200

    fixed merge defects

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express();
 const initChatEndpoint = require("./src/chat.js");
-const setupServerEndpoints = require("./src/server.js");
 const connectToDB = require("./dataAccess.js");
+
+const setupServerEndpoints = (app, express) => {
+    app.get("/", (req, res) => {
+        res.sendFile(__dirname + "/public/chat.html");
+    })
+    
+    app.use(express.static('public'))
+}
 
 setupServerEndpoints(app, express);
 initChatEndpoint(app, 3000);
--- src/server.js
+++
@@ -1,9 +0,0 @@
-const setupServerEndpoints = (app, express) => {
-    app.get("/", (req, res) => {
-        res.sendFile(__dirname + "/public/chat.html");
-    })
-    
-    app.use(express.static('public'))
-}
-
-module.exports = setupServerEndpoints;
